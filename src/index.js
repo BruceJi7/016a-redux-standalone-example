48e23942b54e0fdfae15f42d0430f3535ecfc2cd
BruceJi7 <revolverc24@gmail.com>
2021-01-12 17:17:22 +0900

    Add: Integrate Redux Devtools with Logger Middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import counterReducer from "./store/reducers/counter"
 import resultReducer from "./store/reducers/result"
@@ -15,8 +15,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 })
 
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('Middleware: Dispatching ', action)
+            const result = next(action)
+            console.log('Middleware: Next state is ', store.getState())
+            return result
+        }
+    }
+}
 
-const store = createStore(rootReducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
